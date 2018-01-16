1d120aded0a62a55efa6caa08a806ab22f57da11
Monicha <33831040+Monicha@users.noreply.github.com>
2018-01-16 16:39:23 +0200

    Delete to_top_button.js

--- to_top_button.js
+++
@@ -1,8 +0,0 @@
-$("#scroll_To_Bottom").on("click", function(){
-  $("html, body").animate({scroll_To_Bottom:$("#").offset().bottom}, 3000);
-});
-
-
-$("#scroll_To_Top").on("click", function(){
-  $("html, body").animate({scrollTop:$(".container").offset().top}, 3000);
-});
